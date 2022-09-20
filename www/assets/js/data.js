226ed2cc0bfc63ef46e3df9c64df625a9c1ad3b6
Henrique S. Xavier <hsxavier@gmail.com>
2022-09-20 18:17:30 -0300

    Coloquei site do violentometro as is.

---
+++ www/assets/js/data.js
@@ -0,0 +1,1 @@
+data = '{"total_counts": 40191, "yesterday_counts": 1664, "time_series_plot": "../webpage/www/assets/img/graficos/tweets_agressivos_por_dia_feminino.png", "last_update": "2022-09-20T16:01:15"}'
