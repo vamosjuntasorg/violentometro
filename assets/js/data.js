d44a4c4df5418adc659c350231672592658fbc30
Henrique S. Xavier <hsxavier@gmail.com>
2022-09-24 13:53:28 -0300

    Troquei banner / Troquei paineis do gráfico / Adequei textos ao NIC / Coloquei tablea de dados

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,1 +1,1 @@
-data = '{"total_counts": 40191, "yesterday_counts": 1664, "time_series_plot": "../webpage/www/assets/img/graficos/tweets_agressivos_por_dia_feminino.png", "last_update": "2022-09-20T16:01:15"}'
+data = '{"total_counts": 44906, "yesterday_counts": 984, "time_series_plot": "../webpage/www/assets/img/graficos/tweets_agressivos_por_dia_feminino.png", "last_update": "2022-09-24T12:40:45"}'
