320604baf1c80e0b200e17243aeb95a87bb28c30
Henrique S. Xavier <hsxavier@gmail.com>
2022-09-29 14:25:40 -0300

    Ajustado página p/ projeto solo Vamos Juntas + Atulização dos dados

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,1 +1,1 @@
-data = '{"total_counts": 47576, "yesterday_counts": 920, "time_series_plot": "../webpage/data/tweets_agressivos_por_dia_feminino.png", "last_update": "2022-09-27T04:02:21"}'
+data = '{"total_counts": 49684, "yesterday_counts": 1078, "time_series_plot": "../webpage/data/tweets_agressivos_por_dia_feminino.png", "last_update": "2022-09-29T04:02:30"}'
